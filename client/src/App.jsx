a454382ee49ed0c99a844e2b05712b738c4112f0
Pahima R Uchil <uchilpahi@gmail.com>
2025-10-04 10:17:52 +0530

    Add CreateCampaign & Thirdweb contract connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import {CampaignDetails, CreateCampaign, Home, Profile} from './pages'
-import { Sidebar, Navbar } from './components'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
+import { Sidebar, Navbar } from "./components";
 
 const App = () => {
   return (
-     <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
-      <div className='sm:flex hidden mr-10 relative'>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
-        </div>
+      </div>
 
-
-        <div className='flex-1 max-sm:w-full max-w-[1280] mx-auto sm:pr-5'>
-          <Navbar />
-          <Routes path="/" element={<Home/>}></Routes>
-        </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280] mx-auto sm:pr-5">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,14 +3,18 @@ import { Link, useNavigate } from "react-router-dom";
 import { CustomButton } from "./";
 import { logo, menu, search, thirdweb } from "../assets";
 import { navlinks } from "../constants";
+import { useStateContext } from "../context";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  const address = "0xabc";
+  
+  const {address,connect} = useStateContext();
+  
 
   return (
+    
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
         <input
@@ -34,7 +38,7 @@ const Navbar = () => {
           styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
           handleClick={() => {
             if (address) navigate("create-campaign");
-            else "connect()";
+            else connect();
           }}
         />
         <Link to="./profile">
@@ -51,7 +55,7 @@ const Navbar = () => {
       <div className="sm:hidden flex justify-between items-center relative">
         <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
           <img
-            src={thirdweb}
+            src={logo}
             alt="user"
             className="w-[60%] h-[60%] object-contain"
           />
@@ -104,7 +108,7 @@ const Navbar = () => {
               styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
               handleClick={() => {
                 if (address) navigate("create-campaign");
-                else "connect()";
+                else connect();
               }}
             />
           </div>
